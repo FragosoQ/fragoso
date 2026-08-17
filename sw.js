a68ed679d7fb8ec715d08e9fffdd5a6d8b6c1068
FragosoQ <qualidade@somengil.com>
2026-08-17 12:10:14 +0000

    Sua mensagem aqui

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  * Ao alterar ficheiros, incrementar VERSION para forçar a atualização.
  */
 
-const VERSION = "v13";
+const VERSION = "v14";
 const SHELL_CACHE = `fragoso-bot-shell-${VERSION}`;
 const CDN_CACHE = `fragoso-bot-cdn-${VERSION}`;
 
